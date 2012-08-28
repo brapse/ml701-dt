fb171bd98f97e1b20f5281e2297c76476fcd75df
Sean Braithwaite <brapse@gmail.com>
2012-08-27 23:21:29 -0400

    Tones:
        + working classification
        + added options and modes
        + pickle the model

--- dt/node.py
+++ dt/node.py
@@ -6,7 +6,7 @@ def log2(num):
 
 
 class Node(object):
-    def __init__(self, examples,variable=None, instance=None):
+    def __init__(self, examples,variable=None, value=None):
         """
         Build a dataframe
         """
@@ -14,18 +14,21 @@ class Node(object):
         # pandas dataframe
         self.examples = examples
 
-        self.children = None
+        self.children = []
 
         # Child properties
         self.variable = variable
-        self.instance = instance
+        self.value = value
 
         self.max_ig = None
         self.predict_var = 'poisonous'
 
 
     def values(self, var):
-        return set(self.examples[var].values)
+        if var in self.variables():
+            return set(self.examples[var].values)
+        else:
+            return set([])
 
     def variables(self):
         return set(self.examples.columns)
@@ -45,7 +48,7 @@ class Node(object):
         a = self.entropy(var_a) 
         b = self.conditional_entropy(var_a, var_b)
 
-        print "E(%s): %f, E(%s|%s): %f" % (var_a, a, var_a, var_b, b)
+        #print "E(%s): %f, E(%s|%s): %f" % (var_a, a, var_a, var_b, b)
 
         return a - b
 
@@ -94,22 +97,29 @@ class Node(object):
 
 
     def calculate_max_ig(self):
-        max_ig = ["dunno", 0]
+        max_ig = False
         for var in self.examples.columns:
             if var != self.predict_var:
                 ig = self.information_gain(self.predict_var, var)
                 #print "ig(poisonous, %s) => %f" % (var, ig)
-                if ig > max_ig[1]:
+                if not max_ig or ig > max_ig[1]:
                     max_ig = [var, ig]
 
         return max_ig
 
     
     def split(self, var):
-        def only_attr(attr, value):
+        print "splitting: %s" % len(self.examples)
+        def only_attr(value):
             return Node(self.examples[self.examples[var] == value], var, value)
 
         self.children = map(only_attr, self.values(var))
+
+
+        return self.children
+
+    def pure(self, var):
+        return len(self.values(var)) == 1
 
 
     def classify(self, example, var):
@@ -118,19 +128,14 @@ class Node(object):
         run it through the decision tree and predict the var of example
         """
 
-        if self.is_leaf:
-            # return the instance with the highest probability
-            # get 
-            def to_tupple(instance):
-                return [instance, self.prob(var, instance)]
-
-            options = map(to_tupple, self.values(var))
-            sort(options, key=lambda x: x[0])
-
-            return options[0]
+        if len(self.children) == 0:
+            if self.prob('poisonous', 1) > self.prob('poisonous', 0):
+                return 1
+            else:
+                return 0
         else:
             for child in self.children:
-                if child.instance == example[var]:
+                if child.value == example[child.variable]:
                     return child.classify(example, var)
 
 if __name__ == "__main__":
@@ -138,9 +143,16 @@ if __name__ == "__main__":
     data = pandas.read_csv('data/noisy10_test.ssv', sep=' ',header=1, skiprows=[0,2])
     n = Node(data)
 
+    print "YO:%s" % n.values('poisonous')
+
     print "len(data): %s" % len(data)
     print "p(poisonous): %f " % n.prob('poisonous', 1)
     print "p(poisonous=1^capshape='f'): %f" % n.joint_prob('poisonous', 1, 'capshape', 'f')
     print "entropy(poisonous): %f" % n.entropy("poisonous")
     print "information_gain(poisonous, capshape): %f" % n.information_gain("poisonous", "capshape")
     print "most_ig: (%s => %f)" % (n.most_ig_var, n.most_ig_value)
+
+    n.split(n.most_ig_var)
+
+    for child in n.children:
+        print "child (%s=%s) => (%s@%f)" % (child.variable, child.value, child.most_ig_var, child.most_ig_value)
--- run.py
+++ run.py
@@ -1,43 +1,93 @@
 #!/usr/bin/en python
 
+
+from optparse import OptionParser
 from dt import DecisionTree
+import pandas
+import pickle
 
-class Reader():
-    def __init__(self, file):
-        pass
+usage = "usage: %prog [options] train|test|ham|spam"
 
-def train(file):
-    print "Training: %s" % file
-    for item in reader.items():
-        classifier.train(item)
+parser = OptionParser(usage=usage)
+
+parser.add_option("-i", "--input", dest="input", default="data/noisy10_test.ssv",
+                   help="input file Default: noisy10_test.ssv")
+
+parser.add_option("-f", "--file", dest="filename", default="tree.pkl",
+                   help="persist model to filename. Default: tree.pkl")
+
+parser.add_option("-v", "--view", dest="view", default="normal",
+                   help="Decorate the output. color, color-tokens or normal. Default: normal")
+
+(options, args) = parser.parse_args()
+
+if len(args) != 1:
+    print "requires a mode, either \"train\" or \"test\""
+    exit(1)
+else:
+    mode = args[0]
+
+
+def train():
+    print "Training: %s" % options.filename
+
+    training = pandas.read_csv(options.input, sep=' ',header=1, skiprows=[0,2])
+    tree = DecisionTree(training, 'poisonouse')
+    tree.grow(0.01)
+
+    filename = "tree.pkl"
+    pickle.dump(tree, open(filename, 'wb'))
 
     print "DONE"
 
 
-def test(file):
-    print "Training: %s" % file
+def test():
+    print "Training: %s" % options.input
+
+    testing  = pandas.read_csv(options.input, sep=' ',header=1, skiprows=[0,2])
 
     result = {True: {True: 0, False: 0},
              False: {True: 0, False: 0}}
 
-    for item in reader.items():
-        hypothesis = classifier.classify(item)
+    tp = 0
+    tn = 0
+    fp = 0
+    fn = 0
 
-        result[hypothesis][hypothesis == item.poisonouse] = result[hypothesis][hypothesis == item.poisonouse] + 1
+    tree = pickle.load(open(options.filename, 'rb'))
 
-    accuracy = (result[True][True] + result[False][True]) / len(reader)
+    for x, item in testing.T.iteritems():
+        hypothesis = tree.classify(item, 'poisonous')
+
+        print "poisonous: %s %s" % (item['poisonous'], hypothesis)
+
+        if hypothesis == item['poisonous']:
+            if hypothesis:
+                tp = tp + 1
+            else:
+                tn = tn + 1
+        else:
+            if hypothesis:
+                fp = fp + 1
+            else:
+                fn = fp + 1
+
+            
+    accuracy = (1.0 * tp + tn) / len(testing)
+
     print "Results: " 
-    print "Accuracy:  %d  "  % accuracy
+    print "tp: %f" % tp
+    print "tn: %f" % tn
+    print "fp: %f" % fp
+    print "fn: %f" % fn
+    print "Accuracy:  %f  "  % accuracy
 
 
-def validate(file):
-    for item in reader.items():
-        hypothesis = classifier.classify(item)
 
-
-def run():
-    pass
-
-reader = Reader("noisy10_test.ssv")
-
-
+try:
+    if mode == "train":
+        train()
+    elif mode == "test":
+        test()
+except KeyboardInterrupt:
+    print "EXIT"
